c7319e6c6492f459b0a9900c0422ec43ed1a6906
unknown <akshay.kalambe@spit.ac.in>
2019-02-19 07:50:56 +0530

    git push origin master

---
+++ src/app/signup/SignupComponent.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup ,FormControl ,Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  myForm: FormGroup;
+  
+  constructor(public fb: FormBuilder) {
+  
+    this.myForm = this.fb.group({
+      firstName: [''],
+      lastName: [''],
+      email: [''],
+      password: [''],
+      confirmPassword: ['']
+    });
+  }
+  ngOnInit() {
+  }
+}
