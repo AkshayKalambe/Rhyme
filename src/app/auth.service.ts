0d76a647ae85ac94301360268385c790c42262f2
unknown <akshay.kalambe@spit.ac.in>
2019-04-26 12:28:36 +0530

    all done

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,39 +1,46 @@
-import { Injectable } from '@angular/core';
-import * as firebase from 'firebase/app';
-import 'firebase/auth';
-import { resolve, reject } from 'q';
+import { Injectable } from "@angular/core";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import { resolve, reject } from "q";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class AuthService {
-
-  constructor() { }
+  constructor() {}
 
   login(email: string, password: string) {
-  return firebase.auth().signInWithEmailAndPassword(email, password);
+    return firebase.auth().signInWithEmailAndPassword(email, password);
   }
-  
-  signup(email: string, password: string, first_name: string, last_name: string) {
-    return new Promise((resolve, reject) => {
-     
-      firebase.auth().createUserWithEmailAndPassword(email, password).then((response) => {
-        let randomNumber = Math.floor(Math.random() * 1000)
-        
-        response.user.updateProfile({
-          displayName: first_name + " " + last_name,
-          photoURL:"https://api.adorable.io/avatars/" + randomNumber
-        }).then(() => {
-          resolve(response.user);
-        }).catch((error) => {
-          reject(error);
-        })
-        
-      }).catch((error) => {
-        reject(error);
-      })
-  
-    })
 
-   } 
+  signup(
+    email: string,
+    password: string,
+    first_name: string,
+    last_name: string
+  ) {
+    return new Promise((resolve, reject) => {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then(response => {
+          let randomNumber = Math.floor(Math.random() * 1000);
+
+          response.user
+            .updateProfile({
+              displayName: first_name + " " + last_name,
+              photoURL: "https://api.adorable.io/avatars/" + randomNumber
+            })
+            .then(() => {
+              resolve(response.user);
+            })
+            .catch(error => {
+              reject(error);
+            });
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
   }
+}
